082079d8a8bbaacfb9de461295a50e9f4fdbbc69
Franck Duché <franck@hivebrite.com>
2022-08-08 14:42:02 +0200

    add grpc listening

--- logger-service/cmd/api/main.go
+++ logger-service/cmd/api/main.go
@@ -56,6 +56,9 @@ func main() {
 	err = rpc.Register(new(RPCServer))
 	go app.rpcListen()
 
+	// Register the gRPC Server
+	go app.gRPCListen()
+
 	srv := &http.Server{
 		Addr:    fmt.Sprintf(":%s", webPort),
 		Handler: app.routes(),
